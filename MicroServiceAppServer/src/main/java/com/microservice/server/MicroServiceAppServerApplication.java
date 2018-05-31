f18c5451d8556a4a2c7da1061c95a1d1de9abc5a
Befrdu Gebreamlack <befrdu.gebreamlack@gmail.com>
2018-05-31 14:47:34 -0500

    Bind the proxy gateway with the server

--- MicroServiceAppServer/src/main/java/com/microservice/server/MicroServiceAppServerApplication.java
+++ MicroServiceAppServer/src/main/java/com/microservice/server/MicroServiceAppServerApplication.java
@@ -3,8 +3,10 @@ package com.microservice.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaServer
 @SpringBootApplication
+@EnableZuulProxy
 public class MicroServiceAppServerApplication {
 
 	public static void main(String[] args) {
